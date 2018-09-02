d649d4fb266a1d079a36dc4ad74f5d63ccb92a3f
tongtong <1053673440@qq.com>
2018-09-02 16:34:56 +0800

    update comment

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,10 +5,11 @@
         <img class="img" src="@/assets/img/me.jpg" alt="">
       </div>
       <div class="category">
-        <div><router-link to="/">首页</router-link></div>
-        <div><router-link to="/blog">我的博客</router-link></div>
-        <div><router-link to="/comment">留言板</router-link></div>
-        <div><router-link to="">我的作品集</router-link></div>
+        <div><router-link to="/" title="首页"><i class="iconfont-sm">&#xe61b;</i></router-link></div>
+        <div><router-link to="/blog" title="我的博客"><i class="iconfont-sm">&#xe61c;</i></router-link></div>
+        <div><router-link to="/comment" title="留言板"><i class="iconfont-sm">&#xe744;</i></router-link></div>
+        <div><a href="https://codepen.io/ac-greener/" title="codepen"><i class="iconfont-sm">&#xe65b;</i></a></div>
+        <div><a href="https://github.com/AC-greener" title="github"><i class="iconfont-sm">&#xe6a4;</i></a></div>
       </div>
       <div class="introduceMe">
         <h2>我是朱通通</h2>
--- src/pages/comment/Comment.vue
+++ src/pages/comment/Comment.vue
@@ -8,18 +8,40 @@
           <h1><strong>留言板</strong></h1>
           <h3 style="font-size:24px"><strong>已有100人在此留言</strong></h3>
         </header>
-        <div class="doComment">
+        <form class="doComment" action="" method="post">
           <div>
-            <input type="text" placeholder="您的大名">
+            <img class="img" src="@/assets/img/user.png" alt="用户头像  ">
+            <input style="margin-left:100px; margin-top:-50px; display:block" type="text" placeholder="在此输入您的大名">
           </div>
           <div>
-            <textarea name="" id="" cols="30" rows="10" 我也来说几句></textarea>
+            <textarea name="" id=""  rows="10" style="margin-right:10%; width:100%;" placeholder="我也来说几句" ></textarea>
           </div>
           <div>
             <input type="email" placeholder="Email地址">
             <button>提交</button>
           </div>
-        </div>
+        </form>
+        <div class="peopleCommentWrap">
+          <div>
+            <img class="img" src="@/assets/img/user.png" alt="用户头像  ">
+            <span class="userName">张三</span>
+          </div>
+          <div class="peopleComment">
+            you’re a real cool guy！
+            you’re a real cool guy！
+            you’re a real cool guy！
+            you’re a real cool guy！
+            you’re a real cool guy！
+            you’re a real cool guy！
+            you’re a real cool guy！
+            you’re a real cool guy！
+            you’re a real cool guy！
+          </div>
+          <div class="doLike">
+            <div class="like">喜欢</div>
+            <div class="dislike" style="margin-left:50px">不喜欢</div>
+          </div>
+        </div> 
     </div> 
   </div>
 </template>
@@ -38,11 +60,15 @@ export default {
   * {
       font-family: "merriweather",SimSun,"宋体",serif;
       text-shadow: 0 2px 5px rgba(0,0,0,0.2);
-    }
+  }
+  .img {
+    clip-path: circle(50%);
+    width: 76px;
+    height: 76px;
+  }
   header {
     font-size: 30px;
     text-align: center;
-    border: 1px solid red;
     margin-top: 80px;
     margin-bottom: 60px;
   }
@@ -59,17 +85,62 @@ export default {
   .commentRight {
     margin-left: 35%;
     width: 100%;
-    border: 1px solid yellow;
     display: flex;
     flex-direction: column;
     align-items: center;
+    background-color: #ebeef5;
+    height: 1800px;
   }
   .doComment {
-    border: 1px solid red;
-    height: 300px;
+    width: 80%;
+    display: flex;
+    height: 360px;
+    flex-direction: column;
+    background-color: white;
+    margin: 20px;
+    box-shadow: 2px 2px 5px rgba(0,0,0,0.2);
+  }
+  .doComment div {
+    margin: 10px;
+    margin-left: 40px;
+  }
+  .doComment input, textarea, button {
+    background-color: white;
+    padding: 5px 12px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  input {
+    width: 30%;
+    height: 40px;
+  } 
+  button {
+    height: 40px;
+    width: 10%;
+  }
+
+
+  .peopleCommentWrap {
     width: 80%;
     display: flex;
     flex-direction: column;
+    background-color: white;
+    margin: 30px;
+    box-shadow: 2px 2px 5px rgba(0,0,0,0.2);
+    padding-bottom: 20px;
+  }
+  .peopleCommentWrap div {
+    margin: 10px;
+    margin-left: 40px;
+  }
+  .userName {
+    position: absolute;
+    margin-left: 16px; 
+    margin-top: 26px; 
+  }
+  .doLike {
+    border: 1px solid red;
+    display: flex;
   }
 </style>
 
